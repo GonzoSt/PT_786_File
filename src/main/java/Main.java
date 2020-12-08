353a475e730fa3eb9df91129a9bc25e07fa007df
GonzoSt <zzposte@yandex.ru>
2020-12-08 16:35:11 +0500

    Task_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
-import org.apache.commons.io.FileUtils;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -15,8 +15,11 @@ public class Main {
 
 
 //      1.  Даны две строки. Вывести в консоль все слова из первой строки, которые отсутствуют во второй.
-        System.out.println("Task 4.1: ");
+
         try {
+            PrintWriter writer = new PrintWriter(new FileWriter("results.txt"));
+            writer.println("Task 4.1: ");
+            writer.println("Слова, отсутсвующие во второй строке: ");
             String firstLine = Files.readAllLines(Paths.get(filePathTask_1)).get(0);
             String secondLine = Files.readAllLines(Paths.get(filePathTask_1)).get(1);
 
@@ -33,28 +36,38 @@ public class Main {
                     }
                 }
                 if (!isExistInSecondRow) {
-                    System.out.println(firstStringArr[i]);
+                    writer.println(firstStringArr[i]);
                 }
             }
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(" ");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        writer.println(" ");
 
 //        2. В текстовый файл построчно записаны фамилия и имя учащихся класса и его оценка за контрольную.
 //           Вывести на экран всех учащихся, чья оценка меньше 3 баллов и посчитать средний балл по классу.
-        System.out.println("Task 4.2: ");
-        try {
+            writer.println("Task 4.2: ");
+//        try {
             List<String> readStudies = Files.readAllLines(Paths.get(filePathTask_2));
-//            for (String str : readStudies) {
-//                System.out.println(str);
-//            }
+            int sum = 0;
+            int count = readStudies.size();
+            writer.println("Ученики с оценкой ниже 3:");
+            for (String studies : readStudies) {
+                String[] splitArr = studies.split(" ");
+                int mark = Integer.valueOf(splitArr[2]);
+                if (mark < 3) {
+                    writer.println(String.format("%s %s", splitArr[0], splitArr[1]));
+                }
+                sum += mark;
+            }
+            writer.println("\n" + "Средний балл по классу: ");
+            double avg = sum / (1.0d * count);
+            writer.println(String.valueOf(String.format("%.2f", avg)));
+            writer.flush();
+//            System.out.println("\n" + String.format("%.2f", avg));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
     }
 }
